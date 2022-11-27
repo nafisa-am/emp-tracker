39130f75703f7007b0f9d58bb76756edf38bf8f2
nafisa-am <nafisa.am91@gmail.com>
2022-11-27 23:44:47 +0000

    adding code to schema & seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,28 +1,32 @@
+DROP DATABASE IF EXISTS tracker_db;
+CREATE DATABASE tracker_db;
 
--- * `department`
+USE tracker_db;
 
---     * `id`: `INT PRIMARY KEY`
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  department_name VARCHAR(30) NOT NULL
+);
 
---     * `name`: `VARCHAR(30)` to hold department name
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL (10.5),
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id)
+    ON DELETE SET NULL
+);
 
--- * `role`
-
---     * `id`: `INT PRIMARY KEY`
-
---     * `title`: `VARCHAR(30)` to hold role title
-
---     * `salary`: `DECIMAL` to hold role salary
-
---     * `department_id`: `INT` to hold reference to department role belongs to
-
--- * `employee`
-
---     * `id`: `INT PRIMARY KEY`
-
---     * `first_name`: `VARCHAR(30)` to hold employee first name
-
---     * `last_name`: `VARCHAR(30)` to hold employee last name
-
---     * `role_id`: `INT` to hold reference to employee role
-
---     * `manager_id`: `INT` to hold reference to another employee that is the manager of the current employee (`null` if the employee has no manager)
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id),
+    FOREIGN KEY (manager_id)
+    REFERENCES employees(id)
+    ON DELETE SET NULL
+);
